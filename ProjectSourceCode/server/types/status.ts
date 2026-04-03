9ca47b0bc5301d2720fcba718a7b5b4ebb519ae0
Max Malkin <maxim_malkin@outlook.com>
2026-04-03 14:25:05 -0600

    zod validator for status

--- ProjectSourceCode/server/types/status.ts
+++ ProjectSourceCode/server/types/status.ts
@@ -1,6 +1,10 @@
+import z from "zod";
+
 export const watchStatusValues = [
-  "Watched",
-  "In Progress",
-  "Want to Watch",
+	"Watched",
+	"In Progress",
+	"Want to Watch",
 ] as const;
 export type WatchStatus = (typeof watchStatusValues)[number];
+
+export const WatchStatusSchema = z.enum(watchStatusValues);
