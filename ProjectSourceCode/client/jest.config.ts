8e45ff5ff5e8d7a1822aaca72ad5de4373b55de9
Max Malkin <maxim_malkin@outlook.com>
2026-04-17 12:38:49 -0600

    jester_dancing.gif

---
+++ ProjectSourceCode/client/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEach: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+};
+
+export default createJestConfig(config);
